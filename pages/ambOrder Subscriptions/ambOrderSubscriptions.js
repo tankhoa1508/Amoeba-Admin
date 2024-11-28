071f82944138b1633eff2a23f1574bdfd6eab471
Khoa Nguyen <khoanguyen150820@gmail.com>
2024-11-28 18:00:28 +0700

    create subscription

---
+++ pages/ambOrder Subscriptions/ambOrderSubscriptions.js
@@ -0,0 +1,669 @@
+// Subscriptions courses table
+let subscriptionsCourses = [
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Nearly Expired",
+      class: "nearlyExpired"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Nearly Expired",
+      class: "nearlyExpired"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Nearly Expired",
+      class: "nearlyExpired"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Nearly Expired",
+      class: "nearlyExpired"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Nearly Expired",
+      class: "nearlyExpired"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Nearly Expired",
+      class: "nearlyExpired"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Nearly Expired",
+      class: "nearlyExpired"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Expired",
+      class: "expired"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Expired",
+      class: "expired"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Expired",
+      class: "expired"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Expired",
+      class: "expired"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Expired",
+      class: "expired"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Expired",
+      class: "expired"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Expired",
+      class: "expired"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Expired",
+      class: "expired"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    },
+    {
+      subscriptionID: "#SU.AGV274",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV223",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      subscriptionID: "#SU.AGV300",
+      userID: "#US.AGV274",
+      pluginID: "#PL.AGV250",
+      price: "49.000",
+      duration: "30 days",
+      purchasedDate: "12/08/2024",
+      expiryDate: "12/09/2024",
+      status: "Canceled",
+      class: "canceled"
+    }
+  ]
+
+// Subscriptions courses table
+const _tbodySubscriptions = $("tbody.subscriptions");
+const subscriptionsList = subscriptionsCourses.map(function (item) {
+  _tbodySubscriptions.append(
+    `
+      <tr>
+          <td class="fw-bold">
+            <a href="./ambOrderSubscriptionsDetail.html" class="text-dark">
+              ${item.subscriptionID}
+            </a>
+          </td>
+          <td>
+            <a href="./ambOrderSubscriptionsDetail.html" class="text-dark">
+              ${item.userID}
+            </a>
+          </td>
+          <td>
+            <a href="./ambOrderSubscriptionsDetail.html" class="text-dark">
+              ${item.pluginID}
+            </a>
+          </td>
+          <td>
+            <a href="./ambOrderSubscriptionsDetail.html" class="text-dark">
+              <span class="fw-bold" style="color: #ED1C24;">${item.price}</span>đ
+            </a>
+          </td>
+          <td>
+            <a href="./ambOrderSubscriptionsDetail.html" class="text-dark">
+              ${item.duration}
+            </a>
+          </td>
+          <td class="purchasedDate">
+            <a href="./ambOrderSubscriptionsDetail.html" class="text-dark">
+              ${item.purchasedDate}
+            </a>
+          </td>
+          <td class="expiryDate">
+            <a href="./ambOrderSubscriptionsDetail.html" class="text-dark">
+              ${item.expiryDate}
+            </a>
+          </td>
+          <td>
+            <a href="./ambOrderSubscriptionsDetail.html"  class="${item.class}">
+              ${item.status}
+            </a>
+          </td>
+      </tr>`
+  );
+});
