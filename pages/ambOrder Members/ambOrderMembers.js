44ed89b56daf55aca5d09835225b0f9945c21738
Khoa Nguyen <khoanguyen150820@gmail.com>
2024-12-02 18:55:34 +0700

    add Members page

---
+++ pages/ambOrder Members/ambOrderMembers.js
@@ -0,0 +1,629 @@
+// Members courses table
+let membersCourses = [
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    },
+    {
+      memberID: "#MB.AGV274",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Active",
+      class: "active"
+    },
+    {
+      memberID: "#MB.AGV300",
+      clientName: "Truc Hoang",
+      phoneNumber: "0000000000",
+      email: "truc.hoang@amoeba.group",
+      role: "Content Manager",
+      joinedTime: "01/01/2024",
+      status: "Blocked",
+      class: "blocked"
+    }
+  ]
+
+// Members courses table
+const _tbodyMembers = $("tbody.members");
+const membersList = membersCourses.map(function (item) {
+  _tbodyMembers.append(
+    `
+      <tr>
+          <td class="fw-bold">
+            <a href="./ambOrderMembersDetail.html" class="text-dark">
+              ${item.memberID}
+            </a>
+          </td>
+          <td>
+            <a href="./ambOrderMembersDetail.html" class="text-dark">
+              ${item.clientName}
+            </a>
+          </td>
+          <td>
+            <a href="./ambOrderMembersDetail.html" class="text-dark">
+              +84 <span class="fw-bold">${item.phoneNumber}</span>
+            </a>
+          </td>
+          <td>
+            <a href="./ambOrderMembersDetail.html" class="text-dark">
+              ${item.email}
+            </a>
+          </td>
+          <td>
+            <a href="./ambOrderMembersDetail.html" class="text-dark">
+              ${item.role}
+            </a>
+          </td>
+          <td>
+            <a href="./ambOrderMembersDetail.html" class="text-dark">
+              ${item.joinedTime}
+            </a>
+          </td>
+          <td>
+            <a href="./ambOrderMembersDetail.html"  class="${item.class}">
+              ${item.status}
+            </a>
+          </td>
+      </tr>`
+  );
+});
+
+// function accessModules() {
+//   let list = document.getElementsByClassName('.access-modules');
+//   for (i = 0; i < a.length; i++) {
+//       a[i].classList.remove('active')
+//   }
+//   elem.classList.add('active');
+// }
+
+// const modulesList = document.querySelectorAll('.access-modules');
+
+// modulesList.forEach(module => {
+//   module.addEventListener('click', () => {
+//     document.querySelector('.module-active')?.classList.remove('module-active');
+//     module.classList.add('module-non-active')
+//   })
+// })
+
+$(".access-modules").on("click", function() {
+  $(this).toggleClass("module-active");
+})
